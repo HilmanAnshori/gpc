e62f8e49f2bc365fd76514b57d94d583807eb12b
Hilman Anshori <HilmanAnshori@users.noreply.github.com>
2023-03-05 18:42:31 +0700

    new file:   css/sticky-footer-navbar.css 	new file:   data/Cianjur.json 	new file:   data/cities.json 	new file:   img/logo.png 	new file:   img/pin.png 	new file:   img/pin32.png 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,111 +1,49 @@
 let map;
 
-function initMap() {
+function initMap(kota) {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(-33.91722, 151.23064),
-    zoom: 16,
+    center: new google.maps.LatLng(kota.lat, kota.lng),
+    zoom: 11,
   });
-
-  const iconBase =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
-  const icons = {
-    parking: {
-      icon: iconBase + "parking_lot_maps.png",
-    },
-    library: {
-      icon: iconBase + "library_maps.png",
-    },
-    info: {
-      icon: iconBase + "info-i_maps.png",
-    },
-  };
-  const features = [
-    {
-      position: new google.maps.LatLng(-33.91721, 151.2263),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91539, 151.2282),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91747, 151.22912),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.9191, 151.22907),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91725, 151.23011),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91872, 151.23089),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91784, 151.23094),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91682, 151.23149),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.9179, 151.23463),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91666, 151.23468),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.916988, 151.23364),
-      type: "info",
-    },
-    {
-      position: new google.maps.LatLng(-33.91662347903106, 151.22879464019775),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.916365282092855, 151.22937399734496),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.91665018901448, 151.2282474695587),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.919543720969806, 151.23112279762267),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.91608037421864, 151.23288232673644),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.91851096391805, 151.2344058214569),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.91818154739766, 151.2346203981781),
-      type: "parking",
-    },
-    {
-      position: new google.maps.LatLng(-33.91727341958453, 151.23348314155578),
-      type: "library",
-    },
-  ];
-
+  
   // Create markers.
-  for (let i = 0; i < features.length; i++) {
+  for (let i = 0; i < kota.location.length; i++) {
     const marker = new google.maps.Marker({
-      position: features[i].position,
-      icon: icons[features[i].type].icon,
+      position: new google.maps.LatLng(kota.location[i].lat, kota.location[i].lng),
       map: map,
     });
+    console.log(kota.location[i].lat);
   }
 }
 
-window.initMap = initMap;
+$.getJSON('data/cities.json', function(data) {
+    //console.log(data);
+
+    var text = "";
+    for (let i = 0; i < data.cities.length; i++) {
+        text = text +
+                '<option value="' + i + '">' + data.cities[i] + '</option>';
+    }
+
+    //console.log(text);
+    $("#cities").html(text);
+    
+    var kota = $("#cities option:selected").text()
+    //console.log(kota);
+    getCity(kota);
+});
+
+
+
+//window.initMap = initMap;
+
+function getCity(kota){
+    $.getJSON('data/' + kota + '.json', function(data) {
+        console.log(data);
+    
+        window.initMap = initMap(data);
+        
+    });
+}
+
+
